22bdf315ebf7fc155fc4e7da71b04abcd3b68ccc
saadulkhaf <saadafzal995@gmail.com>
2021-01-13 19:08:44 +0500

    navbar component implemented

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import "./App.css";
+import { BrowserRouter, path, Route, Switch } from "react-router-dom";
+import Header from "./Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">Amazon- clone</header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route path="/login">
+            <h1>Login</h1>
+          </Route>
+          <Route path="/checkout">
+            <h1>Checkout</h1>
+          </Route>
+          <Route path="/">
+            <Header />
+            <h1>Amazon Clone</h1>
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
