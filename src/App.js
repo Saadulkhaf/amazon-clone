8e025ef5b9314022a0aade7c4309dbb4326f6138
saadulkhaf <saadafzal995@gmail.com>
2021-01-14 00:08:23 +0500

    banner implemented

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, path, Route, Switch } from "react-router-dom";
 import Header from "./Header";
+import Home from "./Home";
 
 function App() {
   return (
@@ -10,11 +11,12 @@ function App() {
             <h1>Login</h1>
           </Route>
           <Route path="/checkout">
+            <Header />
             <h1>Checkout</h1>
           </Route>
           <Route path="/">
             <Header />
-            <h1>Amazon Clone</h1>
+            <Home />
           </Route>
         </Switch>
       </div>
---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <img
+        className="home__image"
+        alt=""
+        src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Events/2021/NYNY/Fuji_TallHero_NYNY_en_US_1x._CB412256579_.jpg"
+      />
+    </div>
+  );
+}
+
+export default Home;
